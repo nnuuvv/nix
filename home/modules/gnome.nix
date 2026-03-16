61b506df397fb19336b44210a7328fd15eb76fca
ollie <git@nuv.direct>
2026-03-16 21:18:52 +0100

    adjust gnome config

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -14,7 +14,7 @@
       disable-user-extensions = false;
       enabled-extensions = [
         pkgs.gnomeExtensions."appindicator".extensionUuid
-        pkgs.gnomeExtensions."dash-to-dock".extensionUuid
+        # pkgs.gnomeExtensions."dash-to-dock".extensionUuid
         pkgs.gnomeExtensions."night-theme-switcher".extensionUuid
         pkgs.gnomeExtensions."blur-my-shell".extensionUuid
         pkgs.gnomeExtensions."search-light".extensionUuid
@@ -27,40 +27,15 @@
         "org.wezfurlong.wezterm.desktop"
         "obsidian.desktop"
         "Fluffychat.desktop"
-      ]
-      ++ (
-        if hostname == "Fennekin" then
-          [
-            "com.github.xournalpp.xournalpp.desktop"
-          ]
-        else if hostname == "Ponyta" then
-          [
-          ]
-        else
-          [ ]
-      );
-    };
-    "org/gnome/shell/extensions/dash-to-dock" = {
-      always-center-icons = true;
-      apply-custom-theme = true;
-      background-opacity = 0.80000000000000004;
-      dash-max-icon-size = 48;
-      dock-fixed = false;
-      dock-position = "BOTTOM";
-      extend-height = true;
-      height-fraction = 0.90000000000000002;
-      icon-size-fixed = false;
-      scroll-action = "switch-workspace";
-      show-mounts = false;
-      show-thrash = false;
+      ];
     };
     "org/gnome/desktop/background" = {
-      picture-uri = "file:///home/mar/.local/share/wallpapers/aesthetic-wallpapers/images/pink-clouds.png";
-      picture-uri-dark = "file:///home/mar/.local/share/wallpapers/aesthetic-wallpapers/images/wallhaven-9mjw78.png";
+      picture-uri = "file:///home/liv/.local/share/wallpapers/aesthetic-wallpapers/images/pink-clouds.png";
+      picture-uri-dark = "file:///home/liv/.local/share/wallpapers/aesthetic-wallpapers/images/wallhaven-9mjw78.png";
       picture-options = "zoom"; # "zoom", "centered", "scaled", etc.
     };
     "org/gnome/desktop/screensaver" = {
-      picture-uri = "file:///home/mar/.local/share/wallpapers/aesthetic-wallpapers/images/wallhaven-9mjw78.png";
+      picture-uri = "file:///home/liv/.local/share/wallpapers/aesthetic-wallpapers/images/wallhaven-9mjw78.png";
     };
     "org/gnome/desktop/interface" = {
       # color-scheme = "prefer-light"; # Handled by daynight plugin
@@ -82,7 +57,7 @@
   };
   home.packages = with pkgs; [
     dconf-editor
-    gnomeExtensions.dash-to-dock
+    # gnomeExtensions.dash-to-dock
     gnomeExtensions.night-theme-switcher
     gnomeExtensions.appindicator
     gnomeExtensions.blur-my-shell
