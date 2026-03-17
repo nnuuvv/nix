a99760676d7c7661bfc51fd71b9971d67ad48b03
ollie <git@nuv.direct>
2026-03-17 12:09:48 +0100

    move wezterm config to configs/wezterm.lua and read it in

---
+++ configs/wezterm.lua
@@ -0,0 +1,39 @@
+local get_time_of_day = function()
+    local hour = tonumber(os.date("%H"))
+    if hour >= 6 and hour < 12 then
+        -- morning
+        -- return "Rosé Pine Dawn (Gogh)" -- need to find a good light one
+        return "Catppuccin Mocha"
+    elseif hour >= 12 and hour < 18 then
+        -- afternoon
+        return "Catppuccin Mocha" -- need to find a good light one
+    elseif hour >= 18 and hour < 24 then
+        -- evening
+        return "Catppuccin Mocha"
+    else
+        -- night
+        return "Catppuccin Mocha"
+    end
+end
+
+local wezterm = require('wezterm')
+local config = {}
+
+config.color_scheme = get_time_of_day()
+
+-- config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font 'Maple Mono NF'
+config.font_size = 16
+
+config.window_padding = {
+    left = 5,
+    right = 5,
+    top = 5,
+    bottom = 5,
+}
+
+config.window_close_confirmation = 'NeverPrompt'
+
+-- and finally, return the configuration to wezterm
+return config
--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -3,44 +3,6 @@
 {
   programs.wezterm = {
     enable = true;
-    extraConfig = ''
-       local get_time_of_day = function()
-      	local hour = tonumber(os.date("%H"))
-      	if hour >= 6 and hour < 12 then
-          -- morning
-       		return "Rosé Pine Dawn (Gogh)"
-      	elseif hour >= 12 and hour < 18 then
-          -- afternoon
-       		return "Rosé Pine Dawn (Gogh)"
-      	elseif hour >= 18 and hour < 24 then
-          -- evening
-       		return "tokyonight_night"
-      	else
-          -- night
-       		return "tokyonight_night"
-      	end
-       end
-
-       local wezterm = require('wezterm')
-       local config = {}
-
-       -- config.color_scheme = get_time_of_day()
-
-       config.color_scheme = "Catppuccin Mocha"
-
-       config.font = wezterm.font 'Maple Mono NF'
-
-       config.window_padding = {
-          left = 5,
-          right = 5,
-          top = 5,
-          bottom = 5,
-       }
-
-       config.window_close_confirmation = 'NeverPrompt'
-
-       -- and finally, return the configuration to wezterm
-       return config
-    '';
+    extraConfig = builtins.readFile ../../configs/wezterm.lua;
   };
 }
