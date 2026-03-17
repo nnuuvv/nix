3e3508b31096990f6d306c0b2d3fe954ca60b791
ollie <git@nuv.direct>
2026-03-17 12:09:48 +0100

    rename "powerlevel10k/p10k.zsh" to ".p10k.zsh" as it should be

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -22,56 +22,64 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-        rb = "doas nixos-rebuild switch --flake ~/.dotfiles#$(hostname)";
-        ls = "ls --color";
+      rb = "doas nixos-rebuild switch --flake ~/.dotfiles#$(hostname)";
+      ls = "ls --color";
     };
     history = {
-        size = 5000;
-        append = true;
-        share = true;
-        ignoreSpace = true;
-        ignoreAllDups = true;
-        saveNoDups = true;
-        findNoDups = true;
+      size = 5000;
+      append = true;
+      share = true;
+      ignoreSpace = true;
+      ignoreAllDups = true;
+      saveNoDups = true;
+      findNoDups = true;
     };
 
     zplug = {
-        enable = true;
-        plugins = [
-            { name = "zsh-users/zsh-syntax-highlighting"; }
-            { name = "zsh-users/zsh-completions"; }
-            { name = "zsh-users/zsh-autosuggestions"; }
-            { name = "Aloxaf/fzf-tab"; }
-        ];
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "zsh-users/zsh-completions"; }
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "Aloxaf/fzf-tab"; }
+      ];
     };
-    initContent = let 
-    zshConfigEarlyInit = lib.mkOrder 500 ''
-        if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-        fi
-    ''; 
-    zshConfig = lib.mkOrder 1000 ''
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    initContent =
+      let
+        zshConfigEarlyInit = lib.mkOrder 500 ''
+          if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+              source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+          fi
+        '';
+        zshConfig = lib.mkOrder 1000 ''
+          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-        # Completion styling
-        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-        zstyle ':completion:*' menu no
-        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-    ''; 
-    in lib.mkMerge [ zshConfigEarlyInit zshConfig ];
+          # Completion styling
+          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+          zstyle ':completion:*' menu no
+          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+        '';
+      in
+      lib.mkMerge [
+        zshConfigEarlyInit
+        zshConfig
+      ];
   };
 
+  # TODO: add starship
+
+  # TODO: actually log in n such
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
     flags = [ "--disable-up-arrow" ];
     settings = {
-        auto_sync = true;
-        sync_frequency = "5m";
-        sync_address = "https://atuin.nuv.sh";
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://atuin.nuv.sh";
     };
   };
 
@@ -96,7 +104,7 @@
 
   };
 
-  xdg.configFile."powerlevel10k/p10k.zsh".source = ../../configs/.p10k.zsh;
+  xdg.configFile.".p10k.zsh".source = ../../configs/.p10k.zsh;
 
   home.packages = with pkgs; [
     zsh-powerlevel10k
